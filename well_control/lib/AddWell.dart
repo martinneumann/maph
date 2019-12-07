7f195e153ae01c8abe5a111cd19356d069ef6c92
JeffreyElci <38556052+JeffreyElci@users.noreply.github.com>
2019-12-07 15:25:50 +0100

    Add form well.
    Adds form to create new well.

---
+++ well_control/lib/AddWell.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Settings.dart';
+import 'WellOverview.dart';
+
+class AddWell extends StatefulWidget {
+  AddWell({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _AddWellState createState() => _AddWellState();
+
+}
+
+class _AddWellState extends State<AddWell> {
+  final _formKey = GlobalKey<FormState>();
+  static const wellOverview = "List of Wells";
+  static const wellMap = "Map Overview";
+  static const settings = "Settings";
+
+  static const List<String> menuChoices = <String>[wellOverview, wellMap , settings];
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context) {
+                return menuChoices.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            )
+          ],
+        ),
+        body: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Name of well:"
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Longitude:"
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Longitude:"
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false
+                        // otherwise.
+                        if (_formKey.currentState.validate()) {
+                          // If the form is valid, display a Snackbar.
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(content: Text('Processing Data')));
+                        }
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        )
+      )
+    );
+  }
+
+  void choiceAction(String choice) {
+    if (choice == settings) {
+      Navigator.push(context,
+          MaterialPageRoute(
+              builder: (context) => Settings(title: "Settings")));
+    } else if(choice == wellOverview) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => WellOverview(title: "List of Wells")));
+    }
+    else {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => AddWell(title: "List of Wells")));
+    }
+  }
+}
