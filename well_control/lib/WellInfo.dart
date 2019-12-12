744b2fb7a52d4b46ef8b2a2f75637dde80f27db5
Elsa Buchholz <s0544180@htw-berlin.de>
2019-12-12 22:10:36 +0100

    added in further information well picture and bottum row for call, route to well and report

--- well_control/lib/WellInfo.dart
+++ well_control/lib/WellInfo.dart
@@ -66,12 +66,56 @@ class DisplayWellsInfo extends StatefulWidget {
 class DisplayWellsInfoState extends State<DisplayWellsInfo> {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.report, 'REPORT'),
+        ],
+      ),
+    );
+
     return Scaffold(
 
-        body: Center(
-          child: Text('More info'),
+        body: Column(
+          children: [Image.asset(
+            'images/well_number2.jpg',
+            width: 600,
+            height: 240,
+           //fit: BoxFit.fitHeight,
+          ),
+            buttonSection,
+          ],
         ),
 
     );
+
+
   }
+}
+
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
 }
