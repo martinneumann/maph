b0128a07c522d4145dfcd6e8f40598d22b5eabd1
JeffreyElci <38556052+JeffreyElci@users.noreply.github.com>
2019-12-16 11:01:37 +0100

    Water icon for map. Add water icon for map by downloading a tiff file and use predefined class for integration.
    Removes fontawesome from pubspec and add custom icon.

--- well_control/lib/WellMarker.dart
+++ well_control/lib/WellMarker.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:latlong/latlong.dart';
+import 'package:well_control/assets/water_icon.dart';
 
 class WellMarker {
   String name;
-  static final Icon icon = Icon(FontAwesomeIcons.mapMarkerAlt);
+  static final Icon icon = Icon(WaterIcon.water);
   static final double iconSize = 45.0;
   Marker marker;
   Color markerColor = Color.fromARGB(255, 0, 255, 0);
